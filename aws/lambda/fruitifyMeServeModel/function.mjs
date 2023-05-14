fa30d5126d4a5bf5f270acaa55252ffac773490a
Fady <fady.michel@outlook.com>
2023-05-14 15:57:20 +0300

    trying copying the model with the lambda

--- aws/lambda/fruitifyMeServeModel/function.mjs
+++ aws/lambda/fruitifyMeServeModel/function.mjs
@@ -1,5 +1,5 @@
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+// import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import * as tf from "@tensorflow/tfjs";
 
 const client = new S3Client();
@@ -10,19 +10,19 @@ export async function handler(event, context) {
     Bucket: process.env.MODEL_BUCKET,
     Key: "classNames.json",
   });
-  const modelCommand = new GetObjectCommand({
-    Bucket: process.env.MODEL_BUCKET,
-    Key: "model.json",
-  });
+  // const modelCommand = new GetObjectCommand({
+  //   Bucket: process.env.MODEL_BUCKET,
+  //   Key: "model.json",
+  // });
   try {
     const response = await client.send(classesCommand);
     const str = await response.Body.transformToString();
     const CLASS_NAMES = JSON.parse(str).classNames;
     // Load model
-    const MODEL_URL = await getSignedUrl(client, modelCommand, {
-      expiresIn: 3600,
-    });
-    const model = await tf.loadGraphModel(MODEL_URL);
+    // const MODEL_URL = await getSignedUrl(client, modelCommand, {
+    //   expiresIn: 3600,
+    // });
+    const model = await tf.loadGraphModel("./model.json");
     return { body: CLASS_NAMES, version: JSON.stringify(model.modelVersion) };
   } catch (err) {
     console.error(err);
