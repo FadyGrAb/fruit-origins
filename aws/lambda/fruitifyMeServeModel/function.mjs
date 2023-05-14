27613b1833381f6a80c1115552899261341670f2
Fady <fady.michel@outlook.com>
2023-05-14 14:00:04 +0300

    getting className.json from S3

--- aws/lambda/fruitifyMeServeModel/function.mjs
+++ aws/lambda/fruitifyMeServeModel/function.mjs
@@ -1,4 +1,18 @@
+import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+const client = new S3Client();
+
 export async function handler(event, context) {
+  const command = new GetObjectCommand({
+    Bucket: process.env.MODEL_BUCKET,
+    Key: "classNames.json",
+  });
+  try {
+    const response = await client.send(command);
+    const str = await response.Body.transformToString();
+    console.log(str);
+  } catch (err) {
+    console.error(err);
+  }
   console.log("Hello, It worked");
-  return { message: "Hello It works" };
+  return { body: str };
 }
