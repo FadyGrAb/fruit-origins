782b814061f10ebdd6e69ac2372237331dfa1cdc
Fady <fady.michel@outlook.com>
2023-05-14 15:31:07 +0300

    trying a presigned url for model in lambda

--- aws/lambda/fruitifyMeServeModel/function.mjs
+++ aws/lambda/fruitifyMeServeModel/function.mjs
@@ -1,20 +1,27 @@
 import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import * as tf from "@tensorflow/tfjs";
 
 const client = new S3Client();
 
 export async function handler(event, context) {
   // Get classNames.json
-  const command = new GetObjectCommand({
+  const classesCommand = new GetObjectCommand({
     Bucket: process.env.MODEL_BUCKET,
     Key: "classNames.json",
   });
+  const modelCommand = new GetObjectCommand({
+    Bucket: process.env.MODEL_BUCKET,
+    Key: "*",
+  });
   try {
-    const response = await client.send(command);
+    const response = await client.send(classesCommand);
     const str = await response.Body.transformToString();
     const CLASS_NAMES = JSON.parse(str).classNames;
     // Load model
-    const MODEL_URL = `https://${process.env.MODEL_BUCKET}.s3.amazonaws.com/model.json`;
+    const MODEL_URL = await getSignedUrl(client, modelCommand, {
+      expiresIn: 3600,
+    });
     const model = await tf.loadGraphModel(MODEL_URL);
     return { body: CLASS_NAMES, version: JSON.stringify(model.modelVersion) };
   } catch (err) {
